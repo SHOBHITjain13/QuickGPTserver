2c38d4cabc76b842db2f62e84d5a05f73274e592
SHOBHITjain13 <shobbitjain13@gmail.com>
2025-10-01 15:08:21 +0530

    working on authantication

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import connectDB from "./configs/db.js"
+
+const app = express()
+
+await connectDB();
+
+//M Middleware
+app.use(cors())
+app.use(express.json())
+
+
+// Routes
+app.get('/', (req, res)=> res.send('server is live'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`)
+})
